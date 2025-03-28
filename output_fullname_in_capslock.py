0c80c77a9e66a2c84ffc94f63689d89eee5766c4
Jayvee <bustamantejayvee6@gmail.com>
2025-03-29 00:06:21 +0800

    Changed name

---
+++ output_fullname_in_capslock.py
@@ -0,0 +1,6 @@
+ #Goals:Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+#Get the user input
+user_input = input("Please input your Full name: ")
+#Print all capslocked
+print(user_input.upper())
